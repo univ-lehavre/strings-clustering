dd6073e32b38c779be696457b556747295a26b25
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2025-09-10 09:55:08 +0200

    Ajout de la fonction levenshtein et des tests associés ; mise à jour de la configuration Jest pour inclure les tests dans le répertoire 'tests'

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/src/**/*.test.ts'],
+  testMatch: ['**/src/**/*.test.ts', '**/tests/**/*.test.ts'],
   verbose: true,
 };
 
